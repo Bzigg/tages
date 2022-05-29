82810097103a697b991684d4c122fdec1d681848
no.plehanov <nek211097@yandex.ru>
2022-05-29 20:34:55 +0500

    assets+static

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   head: {
     title: 'tages',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ru'
     },
     meta: [
       { charset: 'utf-8' },
@@ -18,6 +18,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    { src: './assets/scss/style.scss', lang: 'scss' },
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
